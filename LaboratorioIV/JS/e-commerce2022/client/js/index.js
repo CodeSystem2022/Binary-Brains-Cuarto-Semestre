acfa26d91984027eb94f77c5aee11004f6d19885
Mauricio <mauriciosandrea2019@gmail.com>
2023-10-03 15:07:13 -0300

    Ecommerce-clase04

--- LaboratorioIV/JS/e-commerce2022/client/js/index.js
+++ LaboratorioIV/JS/e-commerce2022/client/js/index.js
@@ -1,11 +1,27 @@
-const shopContent = document.getElementById('shopContent');
+const shopContent = document.getElementById("shopContent");
 
-productos.forEach((producto) => {
-    const content = document.createElement('div');
+productos.forEach((product) => {
+    const content = document.createElement("div");
     content.innerHTML = `
-    <img src="${producto.img}">
-    <h3>${producto.productName}</h3>
-    <p>$${producto.price}</p>
-    `;
-    shopContent.appendChild(content);
+    <img src="${product.img}">
+    <h3>${product.productName}</h3>
+    <p>${product.price}</p>`
+    ;
+    shopContent.append(content);
+
+const buyButton = document.createElement("button");
+buyButton.innerText = "Comprar";
+
+content.append(buyButton);
+
+buyButton.addEventListener("click", () => {
+    cart.push({
+        id: product.id,
+        productName: product.productName,
+        price: product.price,
+        quanty: product.quanty,
+        img: product.img,
+    })
+    console.log(cart)
+})
 });
--- LaboratorioIV/JS/e-commerce2022/client/js/products.js
+++ LaboratorioIV/JS/e-commerce2022/client/js/products.js
@@ -1,29 +1,30 @@
 const productos = [
     {
         id: 1,
-        productName: 'Banana',
-        price: 480,
-        quanty : 1,
-        img: 'https://resizer.glanacion.com/resizer/zB6fiKtu_xiC7WqmbAGO8NJpq3E=/880x586/smart/filters:format(webp):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/BJB5RPAYRNEOTK6D7JAHQ6ZCCA.jpg',
+        productName: "Banana",
+        price: 400,
+        quanty: 1,
+        img: "/client/media/banana.png"
     },
     {
         id: 2,
-        productName: 'Leche',
-        price: 450,
-        quanty : 1,
-        img: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/160/839/products/013-003-009_leche-la-serenisma-entera-x-1-litro1-8f1dc924ed9d55a87815885191686834-1024-1024.jpg',
+        productName: "Leche",
+        price: 500,
+        quanty: 1,
+        img: "/client/media/leche.png"
     },
     {
         id: 3,
-        productName: 'Pollo',
-        price: 1750,
-        quanty : 1,
-        img: 'https://www.carnave.com.ar/wp-content/uploads/2020/05/Pollo-entero.jpg',
+        productName: "Pollo",
+        price: 800,
+        quanty: 1,
+        img: "/client/media/pollo.png"
     },
     {
         id: 4,
-        productName: 'Mayonesa',
-        price: 350,
-        quanty : 1,
-        img: 'https://cdn11.bigcommerce.com/s-abmjjefojj/images/stencil/1280x1280/products/2960/12770/Natura-Mayonesa-Limon-500-Argentina_Select-1000x1000__28457.1610887006.jpg?c=1',
-    }];
+        productName: "Mayonesa",
+        price: 200,
+        quanty: 1,
+        img: "/client/media/mayonesa.png"
+    },
+]
